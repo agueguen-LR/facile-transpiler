1600bbde3c4585878ddb5501e47727f38636d915
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2026-02-13 13:26:48 +0100

    Added basic dev environment

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Nix Dev environment for the Facile Transpiler project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        bison
+        cmake
+        flex
+      ];
+
+      shellHook = ''
+
+      '';
+    };
+  };
+}
